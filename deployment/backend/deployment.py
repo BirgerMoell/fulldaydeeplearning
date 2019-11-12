6fa71086833f2c43dcb93b421686d4d5727dc3a6
Anton Osika <anton.osika@gmail.com>
2019-11-12 21:06:57 +0100

    Add model

--- deployment/backend/deployment.py
+++ deployment/backend/deployment.py
@@ -1,28 +1,24 @@
-import torch
 import flask
 from flask import jsonify, request
 from flask_cors import CORS
 
-import utils
-import settings
+from joblib import load
 
 app = flask.Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+model = load("model.joblib")
 
 
 @app.route("/score/<path:text>")
 def hello(text):
 
-    out = model.transform(text)
-    val = float(out)
+    out = model.predict([text])[0]
 
-    return f"{val}"
+    return f"{out}"
 
 
-@app.route('/api',methods = ['POST'])
+@app.route("/api", methods=["POST"])
 def classify():
-      print("hej classifcy")
-      text = request.json['text']
-      out = model(features.view(1, -1))
-      val = float(out)
-      return jsonify(f"{val}")
+    text = request.json["text"]
+    out = model.predict([text])[0]
+    return jsonify(f"{out}")
