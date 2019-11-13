ad543eb27b3cf050c4bbf707469f0170516693db
Birger Moëll <birger.moell@gmail.com>
2019-11-13 22:18:58 +0100

    Updates

---
+++ precache-manifest.8b727525157a6a95e7406d2b83905769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66a2fbebc4491cefedab63e1afe8690",
+    "url": "/fulldaydeeplearning/index.html"
+  },
+  {
+    "revision": "9da0bb26da9b98accebf",
+    "url": "/fulldaydeeplearning/static/css/main.2c0588a6.chunk.css"
+  },
+  {
+    "revision": "98c980c13ceb3fcfef03",
+    "url": "/fulldaydeeplearning/static/js/2.05054e9c.chunk.js"
+  },
+  {
+    "revision": "9da0bb26da9b98accebf",
+    "url": "/fulldaydeeplearning/static/js/main.072a600a.chunk.js"
+  },
+  {
+    "revision": "34d6cf16f5f73152223a",
+    "url": "/fulldaydeeplearning/static/js/runtime-main.bd93a84f.js"
+  }
+]);
