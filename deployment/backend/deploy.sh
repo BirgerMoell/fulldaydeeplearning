9e440f766bfc89799f7b023cf8ab2b259ae6f518
Anton Osika <anton.osika@gmail.com>
2019-11-15 12:54:25 +0100

    Update deploy.sh

--- deployment/backend/deploy.sh
+++ deployment/backend/deploy.sh
@@ -6,8 +6,8 @@ set -ex
 
 # gcloud auth login
 gcloud auth configure-docker
-APP_DOCKER_IMAGE=gcr.io/momentum-project/spam-classifier
 gcloud config set project momentum-project
+APP_DOCKER_IMAGE=gcr.io/momentum-project/spam-classifier
 docker build -t $APP_DOCKER_IMAGE .
 docker push $APP_DOCKER_IMAGE
 gcloud beta run deploy fullday --project momentum-project --image $APP_DOCKER_IMAGE --region us-central1 --platform managed --memory 1024Mi
